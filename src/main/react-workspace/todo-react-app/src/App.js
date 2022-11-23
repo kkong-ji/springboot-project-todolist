d9f2db0a62eb391d7adbfd616a7abe2d25ec5881
unknown <wisejohn950330@gmail.com>
2022-11-23 11:36:14 +0900

    프론트엔드 개발 (Todo 추가, 수정, 삭제)

--- src/main/react-workspace/todo-react-app/src/App.js
+++ src/main/react-workspace/todo-react-app/src/App.js
@@ -1,31 +1,57 @@
 import React from 'react';
 import Todo from './Todo';
-import { Paper, List } from "@material-ui/core"
+import AddTodo from "./AddTodo.js"
+import { Paper, List, Container } from "@material-ui/core"
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items : [ {id:0, title: "Hello World 1", done: true},
-                {id:1, title: "Hello World 2", done: false},
-      ]
+      items : []
     };
   }
 
+  // (1) 함수 추가
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length;  // key를 위한 id 추가
+    item.done = false;  // done 초기화
+    thisItems.push(item); // 리스트에 아이템 추가
+    this.setState({ items: thisItems });  // 업데이트는 반드시 this.setState로 해야됨
+    console.log("items : ", this.state.items);
+  }
+
+  delete = (item) => {
+    const thisItems = this.state.items;
+    console.log("Before Update Items : ", this.state.items)
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({items: newItems}, () => {
+      // 디버깅 콜백
+      console.log("Update Items : ", this.state.items)
+    });
+  }
+
   render() {
-    // 자바스크립트가 제공하는 map함수를 이용해 배열을 반복해 <Todo.../> 컴포넌트 생성
     var todoItems = this.state.items.length > 0 && (
       <Paper style = {{ margin : 16}}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} />
+            <Todo item={item} key={item.id} delete={this.delete}/>
           ))}
         </List>
       </Paper>
     );
 
-    return <div className="App">{todoItems}</div>;
+    // (2) 함수 연결
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add = {this.add} />
+            <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
   }
 }
 
--- src/main/react-workspace/todo-react-app/src/Todo.js
+++ src/main/react-workspace/todo-react-app/src/Todo.js
@@ -1,28 +1,66 @@
 import React from 'react';
-import { ListItem, ListItemText, InputBase, Checkbox } from "@material-ui/core";
+import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
+import { ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton } from "@material-ui/core";
+
 
 class Todo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { item: props.item};
+        this.state = { item: props.item, readOnly: true};
+        this.delete = props.delete;
+      }
+
+      // 함수 추가
+      deleteEventHandler = () => {
+        this.delete(this.state.item)
+      }
+
+      offReadOnlyMode = () => {
+        console.log("Event!", this.state.readOnly)
+        this.setState({ readOnly: false }, () => {
+            console.log("ReadOnly? ", this.state.readOnly)
+        });
+      }
+
+      editEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({ item: thisItem });
+      }
+
+      checkboxEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.done = !thisItem.done;
+        this.setState({ item: thisItem });
       }
 
   render() {
     const item = this.state.item;
     return (
         <ListItem>
-            <Checkbox checked={item.done} />
+            <Checkbox checked={item.done} onChange={this.checkboxEventHandler} disableRipple />
             <ListItemText>
                 <InputBase 
-                    inputProps = {{ "aria-label" : "naked"}}
+                    inputProps = {{ "aria-label" : "naked", readOnly: this.state.readOnly, }}
                     type="text"
-                    id={item.id}
-                    name={item.id}
+                    id={item.id}            // 각 리스트를 구분하려고 id를 연결
+                    name={item.id}          // 각 리스트를 구분하려고 id를 연결
                     value={item.title}
                     multiline={true}
                     fullWidth={true}
+                    onClick={this.offReadOnlyMode}
+                    onChange={this.editEventHandler}
+                    onKeyPress={this.enterKeyEventHandler}
                 />
             </ListItemText>
+
+            <ListItemSecondaryAction>
+                <IconButton 
+                    aria-label="Delete Todo"
+                    onClick={this.deleteEventHandler}>
+                    <DeleteOutlined />
+                </IconButton>
+            </ListItemSecondaryAction>
         </ListItem>
     )
   }
